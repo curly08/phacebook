f5534224c372848fd967d03c8afd15fa41aeb72f
curly08 <mattynoakes08@gmail.com>
2021-12-22 11:53:46 -0500

    Generate Friendship model and its associations with User/Friend

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+  has_many :received_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :received_friends, through: :received_friendships, source: 'user'
   has_many :posts, dependent: :destroy
+
+  def active_friends
+    friends.select { |friend| friend.friends.include?(self) }
+  end
+
+  def pending_friends
+    friends.reject { |friend| friend.friends.include?(self) }
+  end
 end
