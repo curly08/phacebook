27f0da005a2f851d6c2b7baeaf141b0547512053
curly08 <mattynoakes08@gmail.com>
2021-12-21 21:43:52 -0500

    Scaffold Post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    root 'devise/sessions#new'
-  end
   devise_for :users
+  resources :posts
+  devise_scope :user do
+    authenticated :user do
+      root 'posts#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
