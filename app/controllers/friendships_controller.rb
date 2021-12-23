88202d8d60dccbb19e8c410027e614aec0a9ca5a
curly08 <mattynoakes08@gmail.com>
2021-12-23 11:19:39 -0500

    Add respond_to statements

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,20 +2,24 @@ class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.friendships.build(friend_id: params[:user][:friend_id])
 
-    if @friendship.save
-      redirect_to users_path
-    else
-      return
+    respond_to do |format|
+      if @friendship.save
+        format.html { redirect_to users_url, notice: "Friendship was successfully created." }
+        format.json { render :json, status: :created }
+      else
+        format.html { redirect_to users_url, status: :unprocessable_entity, notice: "Friendship was unsuccessfully created." }
+        format.json { render json: @friendship.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     @friendship = current_user.friendships.find(params[:id])
+    @friendship.destroy
 
-    if @friendship.destroy
-      redirect_to users_path
-    else
-      return
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: "Friendship was successfully destroyed." }
+      format.json { head :no_content }
     end
   end
 end
