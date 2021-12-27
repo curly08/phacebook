52776b19dc1d69bdeb6b61aaa13a93c77b3d7a6d
curly08 <mattynoakes08@gmail.com>
2021-12-27 17:30:49 -0500

    Add update and destroy functionality for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,11 @@
 class CommentsController < ApplicationController
   before_action :get_post
+  before_action :set_comment, only: %i[destroy edit update]
   before_action :authenticate_user!
 
-  # GET /posts/new
-  def new
-    @comment = @post.comments.build
-  end
-
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts or /posts.json
   def create
     @comment = @post.comments.build(body: comment_params[:body], user_id: current_user.id)
 
@@ -20,13 +14,12 @@ class CommentsController < ApplicationController
         format.html { redirect_to post_url(@post), notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render 'posts/show', status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
       if @comment.update(comment_params)
@@ -39,7 +32,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     @comment.destroy
 
@@ -51,12 +43,14 @@ class CommentsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def get_post
     @post = Post.find(params[:post_id])
   end
 
-  # Only allow a list of trusted parameters through.
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
   def comment_params
     params.require(:comment).permit(:body)
   end
