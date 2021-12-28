bd639492fac655dcd1d306f1d3f142c6f443dea2
curly08 <mattynoakes08@gmail.com>
2021-12-28 15:34:22 -0500

    Add #edit and #update for users to edit their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: :show
+  before_action :set_user, except: :index
 
   def index
     @users = User.all
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @user.update(profile_params)
+        format.html { redirect_to @user, notice: "Profile was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show; end
@@ -12,5 +26,9 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def profile_params
+    params.require(:user).permit(:name, :birthday, :bio, :color, :animal, :food)
   end
 end
